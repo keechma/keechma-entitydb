cf49b4572338017713aafbf597a5c9b50911a21e
Tibor Kranjcec <tibor@floatingpoint.io>
2020-01-14 22:59:30 +0100

    Extend schema with authorized-posts

--- test/entitydb/entitydb_test.cljs
+++ test/entitydb/entitydb_test.cljs
@@ -1,145 +1,151 @@
 (ns entitydb.entitydb-test
-    (:require
-     [cljs.test :refer-macros [deftest is testing use-fixtures]]
-     [entitydb.entitydb :as edb]
-     [entitydb.query :as q]))
+  (:require
+    [cljs.test :refer-macros [deftest is testing use-fixtures]]
+    [entitydb.entitydb :as edb]
+    [entitydb.query :as q]))
 
 (use-fixtures :once
-  {:before (fn [] (js/console.clear))})
+              {:before (fn [] (js/console.clear))})
 
 (deftest insert []
-  (let [res (edb/insert {} :user {:id 1 :username "Retro"})]
-    (is (= res {:entitydb/store {:user {1 {:id 1 :entitydb/id 1 :username "Retro" :entitydb/type :user}}}}))))
+         (let [res (edb/insert {} :user {:id 1 :username "Retro"})]
+              (is (= res {:entitydb/store {:user {1 {:id 1 :entitydb/id 1 :username "Retro" :entitydb/type :user}}}}))))
 
 (deftest inserting-entity-when-exists-merges-attrs []
-  (let [store {:entitydb/store {:user {1 {:full-name "Mihael Konjevic"}}}}
-        res (edb/insert store :user {:id 1 :username "Retro"})]
-    (is (= res {:entitydb/store
-            {:user
-             {1 {:id 1 :entitydb/id 1 :username "Retro" :full-name "Mihael Konjevic" :entitydb/type :user}}}}))))
+         (let [store {:entitydb/store {:user {1 {:full-name "Mihael Konjevic"}}}}
+               res (edb/insert store :user {:id 1 :username "Retro"})]
+              (is (= res {:entitydb/store
+                          {:user
+                           {1 {:id 1 :entitydb/id 1 :username "Retro" :full-name "Mihael Konjevic" :entitydb/type :user}}}}))))
 
 (deftest insert-many []
-  (let [res (edb/insert-many {} :user [{:id 1 :username "Retro"} 
-                                       {:id 2 :username "Tibor"}])]
-    (is (= res {:entitydb/store {:user {1 {:id 1 :entitydb/id 1 :username "Retro" :entitydb/type :user}
-                                          2 {:id 2 :entitydb/id 2 :username "Tibor" :entitydb/type :user}}}}))))
+         (let [res (edb/insert-many {} :user [{:id 1 :username "Retro"}
+                                              {:id 2 :username "Tibor"}])]
+              (is (= res {:entitydb/store {:user {1 {:id 1 :entitydb/id 1 :username "Retro" :entitydb/type :user}
+                                                  2 {:id 2 :entitydb/id 2 :username "Tibor" :entitydb/type :user}}}}))))
 
-(def data {:username "retro"
-           :id 1
-           :posts {:pageInfo {:hasNextPage true}
-                   :edges [{:cursor "1"
-                            :node {:slug "my-post-1"
-                                   :title "My Post #1"}}
-                           {:cursor "2"
-                            :node {:slug "my-post-2"
-                                   :title "My Post #2"}}]}
-           :twitterProfile {:username "mihaelkonjevic"
-                            :tweetCount 1234
-                            :followerCount 123}
-           :githubProfile {:username "retro"
-                           :repositories {:pageInfo {:hasNextPage false}
-                                          :edges [{:cursor "1"
-                                                   :node {:id 1
-                                                          :name "keechma"
-                                                          :homepage {:url "https://keechma.com"
+(def data
+  {:username       "retro"
+   :id             1
+   :posts          {:pageInfo {:hasNextPage true}
+                    :edges    [{:cursor "1"
+                                :node   {:slug  "my-post-1"
+                                         :title "My Post #1"}}
+                               {:cursor "2"
+                                :node   {:slug  "my-post-2"
+                                         :title "My Post #2"}}]}
+   :authoredPosts [{:slug "my-post-3"
+                    :title "My Post #3"}]
+   :twitterProfile {:username      "mihaelkonjevic"
+                    :tweetCount    1234
+                    :followerCount 123}
+   :githubProfile  {:username     "retro"
+                    :repositories {:pageInfo {:hasNextPage false}
+                                   :edges    [{:cursor "1"
+                                               :node   {:id         1
+                                                        :name       "keechma"
+                                                        :homepage   {:url         "https://keechma.com"
                                                                      :description "Keechma Homepage"}
-                                                          :committers {:pageInfo {:hasNextPage false}
-                                                                       :edges [{:cursor 1
-                                                                                :node {:username "retro"}}
-                                                                               {:cursor 2
-                                                                                :node {:username "tiborkr"}}]}}}
-                                                  {:cursor "2"
-                                                   :node {:id 2
-                                                          :name "entitydb"}}]}}
+                                                        :committers {:pageInfo {:hasNextPage false}
+                                                                     :edges    [{:cursor 1
+                                                                                 :node   {:username "retro"}}
+                                                                                {:cursor 2
+                                                                                 :node   {:username "tiborkr"}}]}}}
+                                              {:cursor "2"
+                                               :node   {:id   2
+                                                        :name "entitydb"}}]}}
 
-           :urls [{:name "Homepage" :url "https://retroaktive.me"}
-                  {:name "Keechma" :url "https://keechma.com"}]
-           :groups [{:name "Keechma Developers"
+   :urls           [{:name "Homepage" :url "https://retroaktive.me"}
+                    {:name "Keechma" :url "https://keechma.com"}]
+   :groups         [{:name    "Keechma Developers"
                      :members {:pageInfo {:hasNextPage false}
-                               :edges [{:cursor 3
-                                        :node {:id 1}}]}}]})
+                               :edges    [{:cursor 3
+                                           :node   {:id 1}}]}}]})
 
 (def tibor-data {:username "tibor"
-                 :id 2
-                 :posts {:pageInfo {:hasNextPage true}
-                         :edges [{:cursor "1"
-                                  :node {:slug "my-post-1"
-                                         :title "My Post #1"}}
-                                 {:cursor "2"
-                                  :node {:slug "my-post-1"
-                                         :title "My Post #1"}}
-                                 {:cursor "3"
-                                  :node {:slug "my-post-2"
-                                         :title "My Post #2"}}]}})
+                 :id       2
+                 :posts    {:pageInfo {:hasNextPage true}
+                            :edges    [{:cursor "1"
+                                        :node   {:slug  "my-post-1"
+                                                 :title "My Post #1"}}
+                                       {:cursor "2"
+                                        :node   {:slug  "my-post-1"
+                                                 :title "My Post #1"}}
+                                       {:cursor "3"
+                                        :node   {:slug  "my-post-2"
+                                                 :title "My Post #2"}}]}})
 
 (def data-2 {:username "tiborkr"
-             :id 2
-             :posts {:pageInfo {:hasNextPage true}
-                     :edges [{:cursor "1"
-                              :node {:slug "my-post-1"
-                                     :title "My Post #1"}}
-                             {:cursor "3"
-                              :node {:slug "my-post-3"
-                                     :title "My Post #3"}}]}})
+             :id       2
+             :posts    {:pageInfo {:hasNextPage true}
+                        :edges    [{:cursor "1"
+                                    :node   {:slug  "my-post-1"
+                                             :title "My Post #1"}}
+                                   {:cursor "3"
+                                    :node   {:slug  "my-post-3"
+                                             :title "My Post #3"}}]}})
 
 (def data-3 {:username "retro"
-             :id 1
-             :posts {:pageInfo {:hasNextPage true}
-                   :edges [{:cursor "1"
-                            :node {:slug "my-post-3"
-                                   :title "My Post #3"}}]}})
+             :id       1
+             :posts    {:pageInfo {:hasNextPage true}
+                        :edges    [{:cursor "1"
+                                    :node   {:slug  "my-post-3"
+                                             :title "My Post #3"}}]}})
 
-(def schema {:user              {:entitydb/relations 
-                                 {:urls           {:entitydb.relation/path [:urls :*]
-                                                   :entitydb.relation/type :url}
-                                  :posts          {:entitydb.relation/path [:posts :edges :* :node]     
-                                                   :entitydb.relation/type :post}
-                                  :group-members  {:entitydb.relation/path [:groups :* :members :edges :* :node]
-                                                   :entitydb.relation/type :user}
-                                  :twitterProfile :twitter-profile
-                                  :githubProfile  :github-profile}
+(def schema {:user              {:entitydb/relations
+                                                     {:urls           {:entitydb.relation/path [:urls :*]
+                                                                       :entitydb.relation/type :url}
+                                                      :authored-posts {:entitydb.relation/path [:authoredPosts :*]
+                                                                       :entitydb.relation/type :post}
+                                                      :posts          {:entitydb.relation/path [:posts :edges :* :node]
+                                                                       :entitydb.relation/type :post}
+                                                      :group-members  {:entitydb.relation/path [:groups :* :members :edges :* :node]
+                                                                       :entitydb.relation/type :user}
+                                                      :twitterProfile :twitter-profile
+                                                      :githubProfile  :github-profile}
                                  :entitydb/processor (fn [item]
-                                                       (if (contains? item :username)
-                                                         (update item :username #(str "USERNAME:" %))
-                                                         item))}
+                                                         (if (contains? item :username)
+                                                           (update item :username #(str "USERNAME:" %))
+                                                           item))}
              :post              {:entitydb/id :slug}
              :url               {:entitydb/id :url}
              :twitter-profile   {:entitydb/id :username}
              :github-profile    {:entitydb/id :username
                                  :entitydb/relations
-                                 {:repositories {:entitydb.relation/type :github-repository
-                                                 :entitydb.relation/path [:repositories :edges :* :node]}}}
-             :github-repository {:entitydb/relations 
+                                              {:repositories {:entitydb.relation/type :github-repository
+                                                              :entitydb.relation/path [:repositories :edges :* :node]}}}
+             :github-repository {:entitydb/relations
                                  {[:committers :edges :* :node] :github-profile}}})
 
 (deftest relations
-  (let [with-schema (edb/insert-schema {} schema)
-        with-data (-> with-schema
-                      (edb/insert :user data)
-                      (edb/insert :user data-2)
-                      (edb/insert :user data-3)
-                      (edb/remove-by-id :post "my-post-3")
-                      (edb/remove-by-id :url "https://keechma.com")
-                      )]
-    (js/console.log (with-out-str (cljs.pprint/pprint with-data)))
-    ;;(js/console.log "------------------------------")
-    ;;(js/console.log (with-out-str (cljs.pprint/pprint (into {} (filter (fn [[ident _]] (= :post (:type ident))) (get-in with-data [:entitydb.relations/reverse]))))))
-    ;;(js/console.log "------------------------------")
-   ;; (js/console.log (with-out-str (cljs.pprint/pprint (edb/get-by-id with-data :user 1))))
-    (let [query [:urls
-                 ;;:posts
-                 :group-members
-                 (q/include :githubProfile 
-                            [(q/include :repositories 
-                                        [(q/include [:committers :edges :* :node])])])]
-          query-1 [(q/switch {:user [:group-members]})]
-          res (edb/get-by-id with-data :user 1 query)]
-      (js/console.log (with-out-str (cljs.pprint/pprint res))))
-    #_(let [query [(q/reverse-include :user [:urls (q/include :posts [(q/reverse-include :user)])]
-                                    )]
-          res (edb/get-by-id with-data :post "my-post-3" query)]
-      ;;(js/console.log (with-out-str (cljs.pprint/pprint res)))
-      )))
+         (let [with-schema (edb/insert-schema {} schema)
+               with-data (-> with-schema
+                             (edb/insert :user data)
+                             (edb/insert :user data-2)
+                             (edb/insert :user data-3)
+                             (edb/remove-by-id :post "my-post-3")
+                             (edb/remove-by-id :url "https://keechma.com")
+                             )]
+              ;;(js/console.log (with-out-str (cljs.pprint/pprint with-data)))
+              ;;(js/console.log "------------------------------")
+              ;;(js/console.log (with-out-str (cljs.pprint/pprint (into {} (filter (fn [[ident _]] (= :post (:type ident))) (get-in with-data [:entitydb.relations/reverse]))))))
+              ;;(js/console.log "------------------------------")
+              ;; (js/console.log (with-out-str (cljs.pprint/pprint (edb/get-by-id with-data :user 1))))
+              (let [query [:urls
+                           ;;:posts
+                           :group-members
+                           (q/include :githubProfile
+                                      [(q/include :repositories
+                                                  [(q/include [:committers :edges :* :node])])])]
+                    query-1 [(q/switch {:user [:group-members]})]
+                    res (edb/get-by-id with-data :user 1 query)]
+            ;;       (js/console.log (with-out-str (cljs.pprint/pprint res)))
+                )
+              #_(let [query [(q/reverse-include :user [:urls (q/include :posts [(q/reverse-include :user)])]
+                                                )]
+                      res (edb/get-by-id with-data :post "my-post-3" query)]
+                     ;;(js/console.log (with-out-str (cljs.pprint/pprint res)))
+                     )))
 
 ;; (def recursive-data
 ;;   {:name "Root"
