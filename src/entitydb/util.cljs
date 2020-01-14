9278bff558715f06a566699c70c7fe1b24142a5d
Tibor Kranjcec <tibor@floatingpoint.io>
2020-01-14 22:58:30 +0100

    Add nth-vals

--- src/entitydb/util.cljs
+++ src/entitydb/util.cljs
@@ -5,3 +5,8 @@
   [pos coll]
   (vec (concat (subvec (vec coll) 0 pos) 
                (subvec (vec coll) (inc pos)))))
+
+(defn nth-vals [a i m]
+  (if (and (map? m) (> i 0))
+    (reduce into a (map (fn [v] (nth-vals a (dec i) v)) (vals m)))
+    (conj a m)))
