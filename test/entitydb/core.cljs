7a1e6ab5fab635d5e55c25ac868d6eb30686c03e
Tibor Kranjcec <tibor@floatingpoint.io>
2020-02-17 16:05:31 +0100

    Test processor

--- test/entitydb/core.cljs
+++ test/entitydb/core.cljs
@@ -794,130 +794,30 @@
         current-note (edb/get-collection with-data :note/list [:links :user])]
     (is (= [expected-data]
            current-note))))
-;{:id 1
-; :username "tibor"
-; :roles ["ADMIN" "EDITOR"]}
-;
-;["ADMIN" "EDITOR" "WRITER"]
-;
-;{:user {:entitydb/relations {:roles {:entitydb.relation/path [:roles :*]
-;                                     :entitydb.relation/processor (fn [r] {:id r})}}}
-; :roles {:entitydb/processor (fn [r] {:id r})}}
 
-#_(deftest relations-between-users
-    (let [with-schema (edb/insert-schema {} data/schema)
-          with-data   (-> with-schema
-                          (edb/insert :user data/user-1-data)
-                          (edb/insert :user data/user-2-data)
-                          (edb/insert-named-item
-                            :post
-                            :current
-                            {:slug  "my-post-3"
-                             :title "My Post #3"})
-                          (edb/insert-named-item
-                            :post :favorite
-                            {:slug  "my-post-4"
-                             :title "My Post #4"})
-                          (edb/insert-collection
-                            :github-repository
-                            :current
-                            data/github-repositories-data-1)
-                          (edb/insert-collection
-                            :github-repository
-                            :archived
-                            data/github-repositories-data-2))
-          ;;_ (log "BEFORE VACUUM")
-          _           (log with-data)
-          with-data'  (edb/vacuum with-data)
-          ;;_ (log "AFTER VACUUM")
-          ;;_ (log with-data')
-          ]
-      (is (= 1 1))))
-
-
-;(deftest relations
-;  (let [with-schema (edb/insert-schema {} schema)
-;        with-data   (-> with-schema
-;                        (edb/insert :user data)
-;                        (edb/remove-by-id :post "my-post-3")
-;                        (edb/remove-by-id :url "https://keechma.com")
-;                        ;;(edb/remove-named :favorite)
-;                        )]
-;              ;;(js/console.log (with-out-str (cljs.pprint/pprint with-data)))
-;              ;;(js/console.log "------------------------------")
-;              ;;(js/console.log (with-out-str (cljs.pprint/pprint (into {} (filter (fn [[ident _]] (= :post (:type ident))) (get-in with-data [:entitydb.relations/reverse]))))))
-;              ;;(js/console.log "------------------------------")
-;              ;; (js/console.log (with-out-str (cljs.pprint/pprint (edb/get-by-id with-data :user 1))))
-;              (let [query   [:urls
-;                             :posts
-;                             :group-members
-;                             (q/include :githubProfile
-;                                        [(q/include :repositories
-;                                                    [(q/include [:committers :edges :* :node])])])]
-;                    query-1 [(q/switch {:user [:group-members]})]
-;                    res-1   (edb/get-by-id with-data :user 1 query-1)
-;                    query-2 [(q/reverse-include :user [:urls (q/include :posts [(q/reverse-include :user)])])]
-;                    res-2   (edb/get-by-id with-data :post "my-post-3" query)
-;                    ])))
-
-
-;; (def recursive-data
-;;   {:name "Root"
-;;    :files {:edges [{:node {:name "File Root: 1"}}]}
-;;    :folders {:edges [{:node {:name "1"
-;;                              :files {:edges [{:node {:name "File 1: 1"}}]}}}
-;;                      {:node {:name "2"
-;;                              :folders {:edges [{:node {:name "2 / 1"}}
-;;                                                {:node {:name "2 / 2"
-;;                                                        :folders {:edges [{:node {:name "2 / 2 / 1"}}
-;;                                                                          {:node {:name "2 / 2 / 2"}}]}}}]}}}]}})
-
-
-;; (def recursive-schema
-;;   {:folder {:entitydb/id :name
-;;             :entitydb/relations {:folders {:entitydb.relation/path [:folders :edges :* :node]
-;;                                            :entitydb.relation/type :folder}
-;;                                  :files {:entitydb.relation/path [:files :edges :* :node]
-;;                                          :entitydb.relation/type  :file}}}
-;;    :file {:entitydb/id :name}})
-
-;(deftest recursive-relations
-;  (let [with-schema (edb/insert-schema {} recursive-schema)
-;        with-data (edb/insert with-schema :folder recursive-data)]
-;    ;;(js/console.log (with-out-str (cljs.pprint/pprint with-data)))
-;    ;;(js/console.log (with-out-str (cljs.pprint/pprint (edb/get-by-id with-data :user 1))))
-;    (let [query [:files (q/recur-on :folders)]
-;          res (edb/get-by-id with-data :folder "Root" query)]
-;      (js/console.log (with-out-str (cljs.pprint/pprint res))))))
-
-;; (def recursive-data-2
-;;   {:name "Foo"
-;;    :enemies [{:name "Qux"
-;;               :enemies [{:name "Bar"}]
-;;               :friends [{:name "Baz"
-;;                          :enemies [{:name "Foo"}
-;;                                    {:name  "Bar"
-;;                                     :friends [{:name "Qux"}]}]}]}]
-;;    :friends [{:name "Bar"
-;;               :enemies [{:name "Qux"}]
-;;               :friends [{:name "Baz"
-;;                          :friends [{:name "Qux"
-;;                                     :enemies [{:name "Bar"}
-;;                                               {:name "Foo"}]}]}]}]})
-
-;; (def recursive-schema-2
-;;   {:person {:entitydb/id :name
-;;             :entitydb/relations {:enemies {:entitydb.relation/type :person
-;;                                            :entitydb.relation/path [:enemies :*]}
-;;                                  :friends {:entitydb.relation/type :person
-;;                                            :entitydb.relation/path [:friends :*]}}}})
-
-
-;; (deftest recursive-relations-2
-;;   (let [with-schema (edb/insert-schema {} recursive-schema-2)
-;;         with-data (edb/insert with-schema :person recursive-data-2)]
-;;     ;;(js/console.log (with-out-str (cljs.pprint/pprint with-data)))
-;;     ;;(js/console.log (with-out-str (cljs.pprint/pprint (edb/get-by-id with-data :user 1))))
-;;     (let [query [(q/recur-on :friends 2) (q/recur-on :enemies 2)]
-;;           res (edb/get-by-id with-data :person "Foo" query)]
-;;       (js/console.log (with-out-str (cljs.pprint/pprint res))))))
+(deftest processor-test
+  (let [with-schema         (edb/insert-schema {} {:user {:entitydb/relations
+                                                          {:roles {:entitydb.relation/path [:roles :*]
+                                                                   :entitydb.relation/type :role}}}
+                                                   :role {:entitydb/processor (fn [r] {:id r})}})
+        with-data           (-> with-schema
+                                (edb/insert :user {:id       1
+                                                   :username "Retro"
+                                                   :roles    ["ADMIN" "EDITOR"]})
+                                (edb/insert :user {:id       2
+                                                   :username "Tibor"
+                                                   :roles    ["EDITOR"]}))
+        expected-admin-role (:id (edb/get-by-id with-data :role "ADMIN"))
+        expected-editor-role (-> with-data
+                                 (edb/get-by-id :user 1 [(q/include :roles)])
+                                 (get-in [:roles 1 :id]))
+        expected-users (-> with-data
+                           (edb/get-by-id :role "EDITOR" [(q/reverse-include :user)])
+                           (get-in [:entitydb.relations/reverse :user :roles])
+                           keys)]
+    (is (= "ADMIN"
+           expected-admin-role))
+    (is (= "EDITOR"
+           expected-editor-role))
+    (is (= '(1 2)
+           expected-users))))
