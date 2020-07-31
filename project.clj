991e855b245a3e9e8601a93f320b3247b51f73a6
Tibor Kranjcec <tibor@floatingpoint.io>
2020-07-31 13:06:53 +0200

    Update description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject keechma/entitydb "0.0.1"
-  :description "Relational data store management system"
+  :description "Client side database and normalization engine"
   :url "https://github.com/keechma/keechma-keechma.entitydb"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
