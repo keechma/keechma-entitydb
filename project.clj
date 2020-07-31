0cd5adf087e58566276cbff45b53fee1fc2def1b
Mihael Konjevic <konjevic@gmail.com>
2020-07-31 17:39:50 +0200

    Fix version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject keechma/entitydb "0.0.2"
+(defproject keechma/entitydb "2.0.2"
   :description "Client side database and normalization engine"
   :url "https://github.com/keechma/keechma-entitydb"
   :license {:name "MIT License"
